f2a230840316dd408b3f99f2db86bfae3ba67bc7
Kozo Nishida <kozo-ni@cg05.naist.jp>
2009-10-16 05:06:41 +0900

    removed screen alias and abbreviated del command

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ ln -s ~/projects/dotfiles/vim/.gvimrc ~/
 ln -s ~/projects/dotfiles/zsh/.zshrc ~/
 ln -s ~/projects/dotfiles/zsh/.zsh ~/
 ln -s ~/projects/dotfiles/tmux/.tmux.conf ~/
+
+mkdir ~/.Trash

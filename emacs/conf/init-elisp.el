41f603b7bb0b6e8705f65386ab79c6dc9aa29258
ニシダ コウゾウ <kozo.nishida@gmail.com>
2010-03-16 00:11:01 +0900

    put lcomp.el location ahead

--- emacs/conf/init-elisp.el
+++ emacs/conf/init-elisp.el
@@ -21,9 +21,9 @@
              (local-set-key "\C-c\C-v" 'describe-variable)
              )))
 
+;; (install-elisp "http://www.geocities.co.jp/SiliconValley-Oakland/7673/lisp/lcomp.el")
+
 ;; (install-elisp-from-emacswiki "anything-complete.el")
 ;; (install-elisp "http://www.bookshelf.jp/elc/moccur-edit.el")
 ;; (install-elisp "http://www.cx4a.org/pub/auto-complete.el")
 ;; (install-elisp "http://svn.coderepos.org/share/lang/elisp/anything-c-moccur/trunk/anything-c-moccur.el")
-;; (install-elisp "http://www.geocities.co.jp/SiliconValley-Oakland/7673/lisp/lcomp.el")
-

eff810f90d75e56a93f31623e729429948ae22d7
Kozo Nishida <kozo.nishida@gmail.com>
2010-03-29 16:18:13 +0900

    added js for shortcut manager

---
+++ chrome/vimchrome.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @ShortcutManager
+// @name Scroll down by 64 pixels
+// @namespace 3v28ZYKQkixa
+// @key j
+// @include *
+// @exclude https://mail.google.com/*
+// @execute ScrollDown(["64"])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Scroll up by 64 pixels
+// @namespace 3v28ZYKQkixa
+// @key k
+// @include *
+// @exclude https://mail.google.com/*
+// @execute ScrollUp(["64"])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Select the left tab
+// @namespace 3v28ZYKQkixa
+// @key h
+// @include *
+// @execute SelectLeftTab([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Select the right tab
+// @namespace 3v28ZYKQkixa
+// @key l
+// @include *
+// @execute SelectRightTab([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Back
+// @namespace 3v28ZYKQkixa
+// @key Shift+h
+// @include *
+// @exclude https://mail.google.com/*
+// @execute Back([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Forward
+// @namespace 3v28ZYKQkixa
+// @key Shift+l
+// @include *
+// @exclude https://mail.google.com/*
+// @execute Forward([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name Close the current tab
+// @namespace 3v28ZYKQkixa
+// @key d
+// @include *
+// @exclude https://mail.google.com/*
+// @execute CloseTab([])
+// ==/UserScript==

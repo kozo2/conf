e2bb20fa20d4e2ca37563a749abf952631e6bc36
Kozo Nishida <kozo-ni@is.naist.jp>
2010-04-25 04:44:23 +0900

    changed way to add load-path

--- emacs/.emacs.el
+++ emacs/.emacs.el
@@ -1,13 +1,7 @@
 ;;; uncomment this line to disable loading of "default.el" at startup
 ;; (setq inhibit-default-init t)
 
-;; http://d.hatena.ne.jp/kazu-yamamoto/20080219/1203423707
-(defun my-load-path (path)
-  (let ((epath (expand-file-name path)))
-    (unless (member epath load-path)
-      (setq load-path (cons epath load-path)))))
-
-(my-load-path "~/projects/conf/emacs/conf")
-(my-load-path "~/.emacs.d")
+(add-to-list 'load-path "~/projects/conf/emacs/conf")
+(add-to-list 'load-path "~/.emacs.d")
 
 (load "init-load")

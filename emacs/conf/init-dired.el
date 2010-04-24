0404a2ce5c6fbd3271c2693eeade5b8c483cd10a
Kozo Nishida <kozo-ni@is.naist.jp>
2010-04-25 04:47:08 +0900

    changed dired-up-directory key

--- emacs/conf/init-dired.el
+++ emacs/conf/init-dired.el
@@ -6,12 +6,11 @@
 
 (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
 
-;; http://www-aos.eps.s.u-tokyo.ac.jp/~takagi/SubversionMemo.html#sec41
 (add-hook 'dired-mode-hook
           (function
            (lambda ()
              (local-set-key "s" 'vc-dir)
-             (local-set-key "-" 'dired-up-directory)
+             (local-set-key ";" 'dired-up-directory)
              (local-set-key "j" 'dired-next-line)
              (local-set-key "k" 'dired-previous-line)
              (local-set-key "q" 'kill-this-buffer)

a270ffc95c4806e15971ccb45bdb1a0f9481462c
Kozo Nishida <kozo-ni@is.naist.jp>
2010-04-25 04:46:04 +0900

    added auto-complete setting

--- emacs/conf/init-completion.el
+++ emacs/conf/init-completion.el
@@ -6,3 +6,7 @@
 (setq dabbrev-case-fold-search nil)
 (setq dabbrev-case-replace nil)
 
+;; http://cx4a.org/software/auto-complete/manual.ja.html
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)

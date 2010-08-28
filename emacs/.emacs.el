55a2b2b8a1b78ab879c157aa2dc02bf5eb49c7a0
Kozo NISHIDA <kozo-ni@is.naist.jp>
2010-08-28 22:00:07 +0900

    just renamed

--- emacs/.emacs.el
+++
@@ -1,7 +0,0 @@
-;;; uncomment this line to disable loading of "default.el" at startup
-;; (setq inhibit-default-init t)
-
-(add-to-list 'load-path "~/projects/conf/emacs/conf")
-(add-to-list 'load-path "~/.emacs.d")
-
-(load "init-load")

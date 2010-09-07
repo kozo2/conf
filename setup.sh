aefff3e7624ce4bf238858c9717c7d3349824094
Kozo NISHIDA <kozo-ni@is.naist.jp>
2010-09-08 00:01:01 +0900

    taglist upgrade

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ git clone git@github.com:kozo2/vim-smartchr.git
 git clone git@github.com:kozo2/vim-quickrun.git
 hg clone https://kozo2@bitbucket.org/kozo2/vim-autocomplpop
 hg clone https://kozo2@bitbucket.org/kozo2/vim-fuzzyfinder
-wget -O ~/.vim/taglist.zip 'http://www.vim.org/scripts/download_script.php?src_id=6769'
+wget -O ~/.vim/taglist.zip 'http://www.vim.org/scripts/download_script.php?src_id=7701'
 cd ~/.vim/
 unzip ~/.vim/taglist.zip

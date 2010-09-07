ce041b6e028f2abacd92fc2cfac20dec371bdd75
Kozo NISHIDA <kozo-ni@is.naist.jp>
2010-09-07 23:55:36 +0900

    added taglist.vim

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
-git clone git@github.com:kozo2/conf.git
 git clone git@github.com:kozo2/snipmate.vim.git
 git clone git@github.com:kozo2/vim-smartchr.git
 git clone git@github.com:kozo2/vim-quickrun.git
 hg clone https://kozo2@bitbucket.org/kozo2/vim-autocomplpop
 hg clone https://kozo2@bitbucket.org/kozo2/vim-fuzzyfinder
+wget -O ~/.vim/taglist.zip 'http://www.vim.org/scripts/download_script.php?src_id=6769'
+cd ~/.vim/
+unzip ~/.vim/taglist.zip

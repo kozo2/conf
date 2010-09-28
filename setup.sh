30823058b96e1af7162a8b0956aa4d2ca153e729
Kozo NISHIDA <kozo-ni@is.naist.jp>
2010-09-28 22:52:41 +0900

    added vim-l9

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ git clone git@github.com:kozo2/ipython.git
 git clone git@github.com:kozo2/junit.git
 git clone git@github.com:kozo2/autopagerize_for_chrome.git
 git clone git@github.com:kozo2/jquery.git
+hg clone https://kozo2@bitbucket.org/kozo2/vim-l9
 hg clone https://kozo2@bitbucket.org/kozo2/vim-autocomplpop
 hg clone https://kozo2@bitbucket.org/kozo2/vim-fuzzyfinder
 wget -O ~/.vim/taglist.zip 'http://www.vim.org/scripts/download_script.php?src_id=7701'

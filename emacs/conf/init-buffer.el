9b244c432b4058a0fdf719ead6da5385b9c4d205
Kozo NISHIDA <kozo-ni@is.naist.jp>
2010-09-28 19:07:46 +0900

    added iswitchb and ido setting and recentf shortcut

--- emacs/conf/init-buffer.el
+++ emacs/conf/init-buffer.el
@@ -1,12 +1,22 @@
 (setq make-backup-files nil)
 
 (ffap-bindings)
+
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 (setq uniquify-ignore-buffers-re "*[^*]+*")
 
+(iswitchb-mode 1)
+(setq read-buffer-function 'iswitchb-read-buffer)
+(setq iswitchb-regexp nil)
+(setq iswitchb-prompt-newbuffer nil)
+
+(ido-mode 1)
+(ido-everywhere 1)
+
 (setq recentf-max-saved-items 1024)
 (require 'recentf-ext)
+(global-set-key (kbd "C-x C-;") 'recentf-open-files)
 
 (require 'dired-x)
 (require 'wdired)

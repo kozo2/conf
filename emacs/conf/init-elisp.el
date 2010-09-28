95213bc215a10935bbb245a5f793894e32daa0e7
Kozo NISHIDA <kozo-ni@is.naist.jp>
2010-09-28 19:09:36 +0900

    changed shortcut key

--- emacs/conf/init-elisp.el
+++ emacs/conf/init-elisp.el
@@ -15,7 +15,7 @@
              (local-set-key "\"" 'electric-pair)
              (local-set-key "(" 'electric-pair)
              (local-set-key "{" 'electric-pair)
-             (local-set-key "\C-c\C-n" 'describe-function)
+             (local-set-key "\C-c\C-d" 'describe-function)
              (local-set-key "\C-c\C-v" 'describe-variable)
              )))
 
@@ -25,8 +25,6 @@
              (local-set-key "\"" 'electric-pair)
              (local-set-key "(" 'electric-pair)
              (local-set-key "{" 'electric-pair)
-             (local-set-key "\C-c\C-n" 'describe-function)
+	     (local-set-key "\C-c\C-d" 'describe-function)
              (local-set-key "\C-c\C-v" 'describe-variable)
              )))
-
-;; (install-elisp "http://www.geocities.co.jp/SiliconValley-Oakland/7673/lisp/lcomp.el")

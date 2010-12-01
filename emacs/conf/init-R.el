91b638b1cd565a028c64fd2116db7750254a05bc
Kozo NISHIDA <kozo-ni@is.naist.jp>
2010-12-02 02:12:15 +0900

    added history and Vim like help move shortcuts

--- emacs/conf/init-R.el
+++ emacs/conf/init-R.el
@@ -1,5 +1,7 @@
 (load "~/projects/ESS/lisp/ess-site")
 
+;(require 'anything-R)
+
 (add-hook 'inferior-ess-mode-hook
           (function
            (lambda ()
@@ -9,7 +11,18 @@
              (local-set-key "[" 'electric-pair)
              (local-set-key "{" 'electric-pair)
 	     (local-set-key "\C-j" 'ess-smart-underscore)
+	     (local-set-key "\C-p" 'comint-previous-input)
+	     (local-set-key "\C-n" 'comint-next-input)
+	     (local-set-key "\C-r" 'comint-history-isearch-backward-regexp)
+;	     (local-set-key "\C-cr" 'anything-for-R)
              )))
+
+(add-hook 'ess-help-mode-hook
+	  (function
+	   (lambda ()
+	     (local-set-key "j" 'ess-skip-to-next-section)
+	     (local-set-key "k" 'ess-skip-to-previous-section)
+	     )))
 
 (add-hook 'ess-mode-hook
           (function
@@ -20,4 +33,5 @@
              (local-set-key "[" 'electric-pair)
              (local-set-key "{" 'electric-pair)
 	     (local-set-key "\C-j" 'ess-smart-underscore)
+;	     (local-set-key "\C-cr" 'anything-for-R)
              )))

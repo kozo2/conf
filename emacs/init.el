b7b358dda52a9b5612bd0cfb81d2049b805925a0
Kozo NISHIDA <kozo-ni@is.naist.jp>
2010-12-06 03:56:14 +0900

    just changed font size

--- emacs/init.el
+++ emacs/init.el
@@ -100,7 +100,7 @@
 
   (set-face-attribute 'default nil
 		      :family "Consolas"
-		      :height 140)
+		      :height 120)
   (set-fontset-font
    nil 'japanese-jisx0208
    (font-spec :family "ＭＳ ゴシック"))

b5b3eddfcfabddec39c1b051bf5f04b625c60e84
Kozo Nishida <knishida@riken.jp>
2011-07-18 01:32:25 +0900

    stop using electric-pair

--- emacs/conf/init-R.el
+++ emacs/conf/init-R.el
@@ -5,15 +5,11 @@
 (add-hook 'inferior-ess-mode-hook
           (function
            (lambda ()
-             (local-set-key "\"" 'electric-pair)
-             (local-set-key "\'" 'electric-pair)
-             (local-set-key "(" 'electric-pair)
-             (local-set-key "[" 'electric-pair)
-             (local-set-key "{" 'electric-pair)
-	     (local-set-key "\C-j" 'ess-smart-underscore)
 	     (local-set-key "\C-p" 'comint-previous-input)
 	     (local-set-key "\C-n" 'comint-next-input)
 	     (local-set-key "\C-r" 'comint-history-isearch-backward-regexp)
+	     (local-set-key "\C-k" 'kill-line)
+	     (local-set-key "\C-j" 'ess-smart-underscore)
 ;	     (local-set-key "\C-cr" 'anything-for-R)
              )))
 
@@ -27,11 +23,6 @@
 (add-hook 'ess-mode-hook
           (function
            (lambda ()
-             (local-set-key "\"" 'electric-pair)
-             (local-set-key "\'" 'electric-pair)
-             (local-set-key "(" 'electric-pair)
-             (local-set-key "[" 'electric-pair)
-             (local-set-key "{" 'electric-pair)
-	     (local-set-key "\C-j" 'ess-smart-underscore)
+	     (local-set-key "\C-a" 'back-to-indentation)
 ;	     (local-set-key "\C-cr" 'anything-for-R)
              )))

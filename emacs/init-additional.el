b3bb4cc8c81bb4a09646e56610c7c0ce1d063a48
Kozo Nishida <knishida@riken.jp>
2011-07-24 03:20:21 +0900

    added bf-mode to auto-install list

--- emacs/init-additional.el
+++ emacs/init-additional.el
@@ -6,13 +6,16 @@
 (setq auto-install-directory "~/.emacs.d/auto-install/")
 ;(setq auto-install-directory "~/elisp/")
 
+
 ;; (auto-install-batch "anything")
 ;; (auto-install-batch "auto-complete development version")
 ;; (auto-install-from-url "https://raw.github.com/m2ym/popwin-el/v0.3/popwin.el")
 ;; (auto-install-from-url "https://raw.github.com/imakado/emacs-smartchr/master/smartchr.el")
+;; (auto-install-from-url "http://www.bookshelf.jp/elc/bf-mode.el")
 ;; (auto-install-from-emacswiki "color-moccur.el")
 ;; (auto-install-from-emacswiki "moccur-edit.el")
 ;; (auto-install-from-emacswiki "grep-edit.el")
+
 
 (require 'anything)
 ;; (require 'anything-startup)

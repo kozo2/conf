0c0a61a5a32d5cd359d0880ca4a14b7c34de43d0
Kozo Nishida <knishida@riken.jp>
2011-08-05 12:01:18 +0900

    moved settings to init-keybind.el

--- emacs/init-function.el
+++ emacs/init-function.el
@@ -1,12 +1,10 @@
 (defun line-to-top-of-window () (interactive) (recenter 0))
-(global-set-key (kbd "C-z") 'line-to-top-of-window)
 
 (defun other-window-or-split ()
   (interactive)
   (when (one-window-p) (split-window-horizontally))
   ;; (when (one-window-p) (split-window-vertically))
   (other-window 1))
-(global-set-key (kbd "C-t") 'other-window-or-split)
 
 ;; http://www.emacswiki.org/cgi-bin/wiki/PythonMode#toc7
 (defun electric-pair ()

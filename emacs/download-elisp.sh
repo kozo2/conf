3751bc7d06afa7ae5fe6f26300894568007526e1
Kozo Nishida <knishida@riken.jp>
2011-08-11 07:06:56 +0900

    added elisp download shellscript

---
+++ emacs/download-elisp.sh
@@ -0,0 +1,11 @@
+#cd ~/.emacs.d/auto-install/
+#curl -O https://raw.github.com/kiwanami/emacs-window-layout/master/window-layout.el
+#wget http://www.emacswiki.org/emacs/download/auto-install.el
+#wget package.el
+
+#cd ~/projects/
+#svn checkout https://svn.r-project.org/ESS/trunk ESS
+#svn checkout http://yasnippet.googlecode.com/svn/trunk/ yasnippet-read-only
+#git clone https://github.com/m2ym/auto-complete.git
+#git clone https://github.com/magit/magit.git
+#git clone https://github.com/kiwanami/emacs-window-manager.git

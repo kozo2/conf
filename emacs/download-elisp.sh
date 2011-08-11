96a220dac6066044ade5f9bff8e7934d715a4b8c
Kozo Nishida <knishida@riken.jp>
2011-08-11 15:18:51 +0900

    just swapped rows

--- emacs/download-elisp.sh
+++ emacs/download-elisp.sh
@@ -1,12 +1,13 @@
 #cd ~/.emacs.d/auto-install/
+#wget http://www.emacswiki.org/emacs/download/auto-install.el
+
 #curl -O https://raw.github.com/kiwanami/emacs-window-layout/master/window-layout.el
 #curl -O https://raw.github.com/m2ym/popwin-el/master/popwin.el
-#wget http://www.emacswiki.org/emacs/download/auto-install.el
-#wget package.el
 
-#cd ~/projects/
+cd ~/projects/
+git clone https://github.com/magit/magit.git
+
 #svn checkout https://svn.r-project.org/ESS/trunk ESS
 #svn checkout http://yasnippet.googlecode.com/svn/trunk/ yasnippet-read-only
 #git clone https://github.com/m2ym/auto-complete.git
-#git clone https://github.com/magit/magit.git
 #git clone https://github.com/kiwanami/emacs-window-manager.git

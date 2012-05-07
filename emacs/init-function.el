c7bbd41bb93ce9105c84a441d584c961bf78a6ff
Kozo Nishida <knishida@riken.jp>
2012-05-07 14:29:59 +0900

    * emacs/init-function.el: added kniker's my-window-resizer

--- emacs/init-function.el
+++ emacs/init-function.el
@@ -37,3 +37,39 @@
 
     (switch-to-buffer-other-window other-buf)
     (other-window -1)))
+
+;; http://d.hatena.ne.jp/khiker/20100119/window_resize
+(defun my-window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        action c)
+    (catch 'end-flag
+      (while t
+        (setq action
+              (read-key-sequence-vector (format "size[%dx%d]"
+                                                (window-width)
+                                                (window-height))))
+        (setq c (aref action 0))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (let ((last-command-char (aref action 0))
+                     (command (key-binding action)))
+                 (when command
+                   (call-interactively command)))
+               (message "Quit")
+               (throw 'end-flag t)))))))

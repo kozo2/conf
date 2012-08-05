99ff06b6110f93367cd48f7313198317da96adb1
Kozo Nishida <knishida@riken.jp>
2012-08-05 12:34:53 +0900

    added many...

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,5 +1,32 @@
 import XMonad
+import XMonad.Prompt
+import XMonad.Prompt.Window
+import XMonad.Util.EZConfig
+import XMonad.Util.NamedScratchpad
+import XMonad.Hooks.FadeInactive
+import Anekos.Config.Layout (myLayoutHook)
+import Anekos.Config.Layout (myLayoutPrompt)
 
-main = xmonad defaultConfig
-	{ terminal = "evilvte -fn \"VL Gothic 14\""
-	}
+scratchpads = [ NS "emacs" "emacs" (className =? "Emacs") nonFloating
+              , NS "google-chrome" "google-chrome" (className =? "Google-chrome") nonFloating
+	      ] where role = stringProperty "WM_WINDOW_ROLE"
+
+myKeys = [ ("M-m", myLayoutPrompt False)
+         , ("M-S-b", windowPromptGoto myXPConfig)
+         , ("M-S-e", namedScratchpadAction scratchpads "emacs")
+         , ("M-S-g", namedScratchpadAction scratchpads "google-chrome")
+         ]
+
+myXPConfig = defaultXPConfig
+             { font = "xft:VL Gothic:pixelsize=12" }
+
+myLogHook :: X ()
+myLogHook = fadeInactiveLogHook fadeAmount where fadeAmount = 0.8
+
+main = xmonad $ defaultConfig
+      { borderWidth = 0
+      , layoutHook  = myLayoutHook
+      , manageHook  = namedScratchpadManageHook scratchpads
+      , logHook     = myLogHook
+      , terminal    = "evilvte -fn \"VL Gothic 14\""
+      } `additionalKeysP` (myKeys)

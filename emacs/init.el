6bb62afdaee41c68081a64e3c9e3dc418b52f64d
Kozo Nishida <knishida@riken.jp>
2013-10-13 20:41:44 +0900

    add font config

--- emacs/init.el
+++ emacs/init.el
@@ -5,9 +5,40 @@
 ;(set-language-environment "Japanese")
 ;(set-language-environment "UTF-8")
 
-;(set-default-font "Menlo 18")
-(add-to-list 'default-frame-alist
-	     '(font . "Monaco-12"))
+(when (and (>= emacs-major-version 24)
+           (eq window-system 'ns))
+  ;; フォントセットを作る
+  (let* ((fontset-name "myfonts") ; フォントセットの名前
+         (size 14) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
+         (asciifont "Menlo") ; ASCIIフォント
+         (jpfont "Hiragino Maru Gothic ProN") ; 日本語フォント
+         (font (format "%s-%d:weight=normal:slant=normal" asciifont size))
+         (fontspec (font-spec :family asciifont))
+         (jp-fontspec (font-spec :family jpfont))
+         (fsn (create-fontset-from-ascii-font font nil fontset-name)))
+    (set-fontset-font fsn 'japanese-jisx0213.2004-1 jp-fontspec)
+    (set-fontset-font fsn 'japanese-jisx0213-2 jp-fontspec)
+    (set-fontset-font fsn 'katakana-jisx0201 jp-fontspec) ; 半角カナ
+    (set-fontset-font fsn '(#x0080 . #x024F) fontspec)    ; 分音符付きラテン
+    (set-fontset-font fsn '(#x0370 . #x03FF) fontspec)    ; ギリシャ文字
+    )
+  
+  ;; デフォルトのフレームパラメータでフォントセットを指定
+  (add-to-list 'default-frame-alist '(font . "fontset-myfonts"))
+
+  ;; フォントサイズの比を設定
+  (dolist (elt '(("^-apple-hiragino.*"               . 1.2)
+		 (".*osaka-bold.*"                   . 1.2)
+		 (".*osaka-medium.*"                 . 1.2)
+		 (".*courier-bold-.*-mac-roman"      . 1.0)
+		 (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+		 (".*monaco-bold-.*-mac-roman"       . 0.9)))
+    (add-to-list 'face-font-rescale-alist elt))
+  
+  ;; デフォルトフェイスにフォントセットを設定
+  ;; # これは起動時に default-frame-alist に従ったフレームが
+  ;; # 作成されない現象への対処
+  (set-face-font 'default "fontset-myfonts"))
 
 (setq dired-dwim-target t)
 (setq debug-on-error t)
